2486a0799ba8b2513ce9b6dab0309cac2ec7e403
Alexander <drumev00@gmail.com>
2019-11-05 23:10:38 +0200

    Solved homework about iterators and comparators

---
+++ Iterators and Comparators/Homework/ListyIteratos/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace ListyIteratos
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ListyIterator<string> iter = new ListyIterator<string>();
+
+			string line = Console.ReadLine();
+
+			while (line != "END")
+			{
+				string[] command = line.Split().ToArray();
+
+				if (command[0] == "Create")
+				{
+					if (command.Length > 1)
+					{
+						iter = new ListyIterator<string>(command.Skip(1).ToArray());
+					}
+					else
+					{
+						iter = new ListyIterator<string>();
+					}
+				}
+				else if (command[0] == "Move")
+				{
+					Console.WriteLine(iter.MoveNext());
+				}
+				else if (command[0] == "HasNext")
+				{
+					Console.WriteLine(iter.HasNext());
+				}
+				else if (command[0] == "Print")
+				{
+					string res = iter.Print();
+
+					Console.WriteLine(res);
+				}
+
+				line = Console.ReadLine();
+			}
+		}
+	}
+}
